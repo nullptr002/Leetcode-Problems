cc2d261c711722db009e1c715aa825f58274f2ae
Galan Lucian <galanlucian002@gmail.com>
2020-09-10 12:43:27 +0300

    Added a better solution to 412; the old one is still there

--- Leetcode Problems/Easy/412. Fizz Buzz.cpp
+++ Leetcode Problems/Easy/412. Fizz Buzz.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 class Solution {
 public:
+    // With division
     vector<string> fizzBuzz(int n) {
         vector<string> sol(n);
 
@@ -18,6 +19,41 @@ public:
                 sol[i - 1] = "Buzz";
             else
                 sol[i - 1] = to_string(i);
+        }
+
+        return sol;
+    }    
+
+    // Without division
+    vector<string> fizzBuzz2(int n) {
+        vector<string> sol(n);
+
+        int i = 0;
+        int fizz = 3, buzz = 5;
+        while (i < n)
+        {
+            if (i + 1 == fizz && i + 1 == buzz)
+            {
+                sol[i] = "FizzBuzz";
+                fizz += 3;
+                buzz += 5;
+            }
+            else if (i + 1 == fizz)
+            {
+                sol[i] = "Fizz";
+                fizz += 3;
+            }
+            else if (i + 1 == buzz)
+            {
+                sol[i] = "Buzz";
+                buzz += 5;
+            }
+            else
+            {
+                sol[i] = to_string(i + 1);
+            }
+
+            i++;
         }
 
         return sol;
